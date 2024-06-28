c0565ed55eeeebd5d15b1eee58be6557aeb9be56
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2024-06-28 15:20:41 +1000

    ENH: link to actual data file in the download script

--- setup_scripts/download_files.sh
+++ setup_scripts/download_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Check if the file doesn't exist before downloading
-fname=sampled.md
+fname=apes_112.tar.gz
 if [ ! -f "/home/user/workshop/data/$fname" ]; then
-    wget -O "/home/user/workshop/data/$fname" "https://www.dropbox.com/scl/fi/cox0ygzgcmdab257hikly/sample.md?rlkey=rad5dahu0nwhbrmjyx9od7mi3&dl=0"
+    wget -O "/home/user/workshop/data/$fname" "https://www.dropbox.com/s/swak1tjpvn1u5qv/apes_112.tar.gz?dl=0"
 fi
